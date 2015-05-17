0b77b8c58b25ce0bbfce932e40d8751395ba0053
Axel Hawker <axelhawker@gmail.com>
2015-05-17 14:07:25 -0500

    Update index.php
    modified connection string

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","axel_moneybags","3^PVtkW]dHw,","axel_monopoly");
+$con=mysqli_connect("localhost","username","password","database");
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
